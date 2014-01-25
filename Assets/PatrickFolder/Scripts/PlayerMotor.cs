edf98c822d566162801e5f917fb91eb5f15b41ca
patrick rasmussen <mcman407@gmail.com>
2014-01-25 09:29:07 -0500

    Player in

---
+++ Assets/PatrickFolder/Scripts/PlayerMotor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotor: MonoBehaviour {
+	private float currentYAngle = 0;
+	public float speed = 3.0f;
+	public float rotateSpeed = 90.0f;
+	
+
+
+	
+	void Update () 
+	{
+		float move = -speed * Input.GetAxis("Vertical") * Time.deltaTime;
+		
+		currentYAngle += rotateSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+
+		this.transform.Translate(Vector3.up * move);
+
+		this.transform.eulerAngles = new Vector3(90,currentYAngle, 0);
+		
+	}
+	
+}
