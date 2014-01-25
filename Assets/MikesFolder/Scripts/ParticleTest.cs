fe9b74fa1b5cdd1e4e26d10a6ef995453466ff81
Travis <tfort34@gmail.com>
2014-01-25 17:43:55 -0500

    change particle

--- Assets/MikesFolder/Scripts/ParticleTest.cs
+++ Assets/MikesFolder/Scripts/ParticleTest.cs
@@ -9,7 +9,7 @@ public class ParticleTest : MonoBehaviour
 					  myXObject;
 	private GameObject myObject;
 		
-	private string key;
+	private string key = "";
 	
 	// Use this for initialization
 	void Start () 
@@ -18,34 +18,33 @@ public class ParticleTest : MonoBehaviour
 
 	void Update()
 	{
-		if (Input.GetKeyDown("z")) /// wanted to turn this into a case to detect different keys, lik z,x,c,v etc
+		if (Input.GetKeyDown(KeyCode.Z)) /// wanted to turn this into a case to detect different keys, lik z,x,c,v etc
 		{
 			key = "z";
 			myObject = myZObject;
 		}
 
-		if (Input.GetKeyDown("x")) /// wanted to turn this into a case to detect different keys, lik z,x,c,v etc
+		if (Input.GetKeyDown(KeyCode.X)) /// wanted to turn this into a case to detect different keys, lik z,x,c,v etc
 		{
 			key = "x";
 			myObject = myXObject;
 		}
 
-		if (Input.GetKeyDown("c")) /// wanted to turn this into a case to detect different keys, lik z,x,c,v etc
+		if (Input.GetKeyDown(KeyCode.C)) /// wanted to turn this into a case to detect different keys, lik z,x,c,v etc
 		{
 			key = "c";
 			myObject = myCObject;
 		}
-
-		Test ();
+		if (key != "")
+			Test ();
 	}
 	
 	void Test()
 	{
 
-		if (Input.GetKey(key) && Input.GetKeyDown ("space"))  // trying use the key variable in the input.getkey is not working
+		if (Input.GetKey(key) && Input.GetKeyDown (KeyCode.Space))  // trying use the key variable in the input.getkey is not working
 		{
 			Instantiate(myObject, transform.position + new Vector3( 40, 25, 0), transform.rotation);	
-			print ("space");
 		}
 	}
 }
