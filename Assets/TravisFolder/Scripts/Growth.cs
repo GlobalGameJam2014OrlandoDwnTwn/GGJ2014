59a2a7e09f017b133e7bf7cdf7fcfe4066d2eb6c
Travis <tfort34@gmail.com>
2014-01-25 12:11:45 -0500

    testing grid space

---
+++ Assets/TravisFolder/Scripts/Growth.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Growth : MonoBehaviour {
+
+	public bool inRange;
+	float timer;
+
+	public GameObject Atom;
+
+	// Use this for initialization
+	void Start () {
+		ResetTimer ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if (inRange && timer <= 0) {
+			ResetTimer();
+			Grow();
+		}
+
+		if (timer <= 0){
+			ResetTimer();
+		}
+	}
+
+	void ResetTimer() {
+		timer = Random.value;
+	}
+
+	void Grow() {
+		Instantiate (Atom, transform.position + new Vector3((int)(-5 + Random.value * 10), (int)(-5 + Random.value * 10), 1), transform.rotation);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("IN");
+			inRange = true;
+		}
+	}
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			StartCoroutine("Leave");
+		}
+	}
+
+	IEnumerator Leave() {
+		yield return new WaitForSeconds(3);
+		inRange = false;
+	}
+
+}
---
+++ Assets/TravisFolder/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public float rotationSpeed = 200; // how fast the player rotates
+	public float vAcceleration = 5; // how fast the player moves (not true acceleration, more like velocity)
+	
+	void Start () {
+	}
+	
+	void Update () {
+		// Controls: WASD or Arrow Keys
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+			transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+			transform.Rotate(Vector3.forward, -rotationSpeed * Time.deltaTime);
+		
+		if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W))
+			Accelerate(vAcceleration);
+		if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S))
+			Accelerate(-vAcceleration);
+		
+	}
+	
+	void Accelerate (float a) {
+		float tTheta = transform.rotation.eulerAngles.z + 90; // offset for local space
+		transform.position += new Vector3(Mathf.Cos (tTheta * Mathf.Deg2Rad), Mathf.Sin (tTheta * Mathf.Deg2Rad), 0) * a * Time.deltaTime;
+	}
+}
