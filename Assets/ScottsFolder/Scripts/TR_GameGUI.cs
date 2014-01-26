861ee669d3b3c0f1af9befb70e2346800b304688
Zephemus <zephemus@gmail.com>
2014-01-26 11:59:25 -0500

    Display texture for Score

---
+++ Assets/ScottsFolder/Scripts/TR_GameGUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TR_GameGUI:MonoBehaviour
+{
+	//Declare score object.
+	public Texture gameScore;
+
+	void OnGUI()
+	{
+		//Instantiate score object.
+		GUI.Label(new Rect(10, 10, gameScore.width / 4, gameScore.height / 4), gameScore);
+	}
+}
