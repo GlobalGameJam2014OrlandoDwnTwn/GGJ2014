79c222f62dd1a586a8f007cc3a5918c7b2a6c104
Zephemus <zephemus@gmail.com>
2014-01-26 12:25:17 -0500

    Score and Combo GUI instantiated

--- Assets/ScottsFolder/Scripts/TR_GameGUI.cs
+++ Assets/ScottsFolder/Scripts/TR_GameGUI.cs
@@ -6,9 +6,21 @@ public class TR_GameGUI:MonoBehaviour
 	//Declare score object.
 	public Texture gameScore;
 
+	//Declare the combo object.
+	public Texture gameCombo;
+
+	//Declare the multiplier object.
+	public Texture gameMulti;
+
 	void OnGUI()
 	{
 		//Instantiate score object.
 		GUI.Label(new Rect(10, 10, gameScore.width / 4, gameScore.height / 4), gameScore);
+
+		//Instantiate combo object.
+		GUI.Label(new Rect(400, 15, gameCombo.width / 5, gameCombo.height / 5), gameCombo);
+
+		//Instantiate the multiplier object.
+		GUI.Label (new Rect(550, 15, gameMulti.width / 5, gameMulti.height / 5), gameMulti);
 	}
 }
