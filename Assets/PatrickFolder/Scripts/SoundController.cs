e5bdd96ec351419ed5e1278b32061169651af0e1
Patrick Rasmussen <mcman407@gmail.com>
2014-01-26 13:41:32 -0500

    update sound controller

---
+++ Assets/PatrickFolder/Scripts/SoundController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+
+	public AudioClip [] notes;
+	private float 	 clipTimer;
+	private int 	 clipCount;
+	// Use this for initialization
+	void Start () 
+	{
+		clipTimer = 0.0f;
+		clipCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		clipTimer -= Time.deltaTime;
+		if(clipTimer < 0.0f)
+		{
+			clipTimer = 0.0f;
+			clipCount = clipCount - Random.Range(0,clipCount);
+		}
+
+	}
+	public void StartSound()
+	{
+		clipTimer += 2.0f;
+		clipCount++;
+		PlayClip();
+	}
+	public void PlayClip()
+	{
+		this.audio.PlayOneShot(notes[clipCount]);
+	}
+}
