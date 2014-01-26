76bc9d92c83c99e715cedd41d286c7f49c05bd4a
Zephemus <zephemus@gmail.com>
2014-01-26 11:43:57 -0500

    MOAR of Scott's Shit

---
+++ Assets/ScottsFolder/Scripts/TR_StartMenuGUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TR_StartMenuGUI:MonoBehaviour
+{
+	//Declare the game background.
+	public Texture gameBackground;
+
+	//Declare the start title.
+	public Texture startTitle;
+
+	//Declare the play button.
+	public Texture playButton;
+
+	void OnGUI()
+	{
+		//Instantiate the game background.
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), gameBackground);
+
+		//Instantiate the game title.
+		GUI.Label(new Rect (300, 10, startTitle.width, startTitle,height), startTitle);
+
+		//Instantiate the play button.
+		GUI.Button(new Rect(500, 300, playButton.width, playButton.height), playButton);
+
+		//Check to see if the play button is pressed.
+			//Load the main play scene.
+	}
+}
